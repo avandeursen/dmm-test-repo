d7994b24144f56a71484e7411033b683ba2354fa
Arie van Deursen <arie.vandeursen@tudelft.nl>
2020-03-17 17:27:09 +0100

    Add second file

---
+++ DMM2.java
@@ -0,0 +1,30 @@
+public class DMM {
+
+  public void hello() {
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	a = 1;
+	b = 2;
+  }
+
+  public void world() {
+	c = 3;
+  }
+
+}
